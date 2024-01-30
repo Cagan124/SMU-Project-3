5d375c290e41d7ba59f68f8399d538427d909846
halej2 <joshua_hale@ymail.com>
2024-01-29 22:18:00 -0600

    Database Notebook and start to Flask API

---
+++ app/app.py
@@ -0,0 +1,36 @@
+# Import the dependencies.
+from flask import Flask, jsonify, render_template
+import pandas as pd
+from sqlHelper import SQLHelper
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+sqlHelper = SQLHelper() # initialize the database helper
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+@app.route("/api/v1.0/<house_type>")
+def get_data(house_type, price, num_beds, num_baths, price_by_sqft, distance):
+    print(house_type, price, num_beds, num_baths, price_by_sqft, distance)
+
+    # execute the queries
+    data_map = sqlHelper.getMapData(house_type, price, num_beds, num_baths, price_by_sqft, distance)
+    # data_bar = sqlHelper.getBarData(house_type)
+
+    data = {"map_data": data_map}
+
+    return jsonify(data)
+
+# @app.route("/about_us")
+# def about_us():
+#     return render_template("about_us.html")
+
+#################################################
+# Execute the App
+#################################################
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app/sqlHelper.py
@@ -0,0 +1,104 @@
+import sqlalchemy
+from sqlalchemy import create_engine, func, inspect, text
+import pandas as pd
+
+class SQLHelper():
+
+    def __init__(self):
+        self.engine = create_engine("sqlite:///Resources/NYC_House_db.sqlite")
+
+    def getMapData(self, house_type, price, num_beds, num_baths, price_by_sqft, distance):
+    
+        # allow the user to select ALL or a specific house type
+        if house_type == "All":
+            where_clause1 = "1=1"
+        else:
+            where_clause1 = f"House_Type = '{house_type}'"
+        # Allow the user to select ALL or specify a maximum price
+        if price is not None:
+            where_clause2 = f"PRICE <= {price}"
+        else:
+            where_clause2 = "1=1"
+        # Allow the user to select ALL or specify a maximum number of bedrooms
+        if num_beds is not None:
+            where_clause3 = f"BEDS <= {num_beds}"
+        else:
+            where_clause3 = "1=1"
+        # Allow the user to select ALL or specify a maximum number of bathrooms
+        if num_baths is not None:
+            where_clause4 = f"BATH <= {num_baths}"
+        else:
+            where_clause4 = "1=1"
+        # Allow the user to select ALL or specify a maximum price per square foot
+        if price_by_sqft is not None:
+            where_clause5 = f"PRICE_BY_SQFT <= {price_by_sqft}"
+        else:
+            where_clause5 = "1=1"
+        # Allow the user to select ALL or specify a maximum distance
+        if distance is not None:
+            where_clause6 = f"Distance <= {distance}"
+        else:
+            where_clause6 = "1=1"
+
+        where_clause = f"{where_clause1} AND {where_clause2} AND {where_clause3} AND {where_clause4} AND {where_clause5} AND {where_clause6}"
+
+        query = f"""
+                SELECT
+                    *
+                FROM
+                    NYC_Houses
+                WHERE
+                    {where_clause};
+        """
+        print(query)
+
+        df_map = pd.read_sql(text(query), con=self.engine)
+        data_map = df_map.to_dict(orient="records")
+
+        return(data_map)
+    
+    # def getBarData(self, house_type, price, num_beds, num_baths, price_by_sqft, distance):
+    #      # allow the user to select the filters for the house they are looking for
+    #     if house_type == "All":
+    #         where_clause1 = "1=1"
+    #     else:
+    #         where_clause1 = f"House_Type == '{house_type}'"
+            
+    #     if price == "All":
+    #         where_clause2 = "1=1"
+    #     else:
+    #         where_clause2 = f"PRICE <= {price}"
+            
+    #     if num_beds == "All":
+    #         where_clause3 = "1=1"
+    #     else:
+    #         where_clause3 = f"BEDS <= {num_beds}"
+            
+    #     if num_baths == "All":
+    #         where_clause4 = "1=1"
+    #     else:
+    #         where_clause4 = f"BATH <= {num_baths}"
+
+    #     if price_by_sqft == "All":
+    #         where_clause5 = "1=1"
+    #     else:
+    #         where_clause5 = f"PRICE_BY_SQFT <= {price_by_sqft}"
+            
+    #     if distance == "All":
+    #         where_clause6 = "1=1"
+    #     else:
+    #         where_clause6 = f"Distance <= {distance}"
+            
+    #     query = f"""
+    #             SELECT
+    #                 *
+    #             FROM
+    #                 NYC_Houses
+    #             WHERE
+    #                 {where_clause1} AND {where_clause2} AND {where_clause3} AND {where_clause4} AND {where_clause5} AND {where_clause6};
+    #     """
+
+    #     df_bar = pd.read_sql(text(query), con=self.engine)
+    #     data_bar = df_bar.to_dict(orient="records")
+
+    #     return(data_bar)
